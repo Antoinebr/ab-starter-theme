0438fe8ba6511d98515e6d74248eacdd3aa56d64
Antoine Brossault <contact@antoinebrossault.com>
2016-03-30 22:09:05 +0200

    alignement WYSWYG WP

--- home.php
+++ home.php
@@ -7,7 +7,7 @@
 
       <?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
 
-        <article>
+        <article class="<?php post_class();?>">
 
           <a href="<?php the_permalink();?>">
             <h1><?php the_title(); ?></h1>
--- templates/components/template-pagination.php
+++ templates/components/template-pagination.php
@@ -1,7 +1,7 @@
 <?php if(get_previous_posts_page_link() && is_paged()): ?>
-  <li class="button button--secondary previous"><a href="<?= get_previous_posts_page_link();?>">&larr; Précédent</a></li>
+  <li><a href="<?= get_previous_posts_page_link();?>">&larr; Précédent</a></li>
 <?php endif; ?>
 
 <?php $url = get_next_posts_page_link(); if($url !== "" && Helpers::others_page_exists()): ?>
-  <li class="button button--secondary next"><a href="<?=$url;?>">Suivant &rarr;</a></li>
+  <li><a href="<?=$url;?>">Suivant &rarr;</a></li>
 <?php endif; ?>
