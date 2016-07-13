bfe61434e30acd4405ea6f7bd70f545d45559b59
Antoine Brossault <contact@antoinebrossault.com>
2016-07-13 16:14:27 +0200

    fix form contact + pagination

--- includes/ajax/form_contact.php
+++ includes/ajax/form_contact.php
@@ -56,9 +56,35 @@ function form_contact() {
   if(!isset($erreur)){
 
     $t['erreur'] = false;
-    $t['successMessage'] = "<p class='sucess'>Inscription réussie</p>";
+
+    $t['erreur'] = false;
+
+
+    $destinataire = get_option('admin_email');
+    $mailSujet = 'Nouvelle demande  depuis le formulaire de contact';
+    $message = "
+
+    <strong>PRENOM : </strong>".$prenom."<br/><br/>
+    <strong>NOM : </strong>".$nom."<br/><br/>
+    <strong>EMAIL : </strong>".$email."<br/><br/>
+    <strong>TELEPHONE : </strong>".$telephone."<br/><br/>
+    <strong>PROFESSION : </strong>".$profession."<br/><br/>
+    <strong>ANCIENETE : </strong>".$ancienete."<br/><br/>
+    <strong>MESSAGE : </strong> ".$message."<br/><br/>
+
+    ";
+
+    $headers[] = "Content-Type: text/html; charset=UTF-8";
+    $headers[] = "From: Franc Muller Site <noreply@nomDuSite.com>";
+
+    wp_mail($destinataire, $mailSujet, $message, $headers);
+
+    $t['successMessage'] = "<p class='sucess u-txtCenter'>Message envoyé</p>";
+
     echo json_encode($t);
+
     die();
+
 
   }elseif($erreur == true){
 
@@ -69,68 +95,6 @@ function form_contact() {
   }
 
 
-  if($t['erreur'] == false){
+}
 
-    $destinataire = "formations@arcphar.com";
-    $mailSujet = 'Nouvelle demande  depuis le formulaire de contact';
-    $message = "
-    <strong>NOM : </strong>".$nom."<br/><br/>
-    <strong>PRENOM : </strong>".$prenom."<br/><br/>
-    <strong>FONCTION : </strong>".$fonction."<br/><br/>
-    <strong>TELEPHONE : </strong>".$telephone."<br/><br/>
-    <strong>EMAIL : </strong>".$email."<br/><br/>
-    <strong>MESSAGE : </strong> ".$message."<br/><br/>
-    ";
-
-    $headers[] = 'Content-Type: text/html; charset=UTF-8';
-    $headers[] = "From: A.R.C. Pharma Formations <noreply@arcphar.com>";
-
-    wp_mail($destinataire, $mailSujet, $message, $headers);
-
-    // Email internaute
-    $baseUrl = get_bloginfo('url');
-    $internauteEmailSujet = "A.R.C. Pharma Formations - Votre demande de renseignement";
-
-    $internauteEmailBody = "
-    A.R.C. Pharma Formations <br/>
-    41 rue de Villiers<br/>
-    92523 Neuilly-sur-Seine Cedex<br/>
-    <br/>
-    <br/>
-    Madame, Monsieur,<br/>
-    <br/>
-    Nous vous remercions de l’intérêt que vous portez à nos programmes de formations.
-    <br/>    <br/>
-    Votre demande a bien été prise en compte ; vous recevrez une réponse dans les meilleurs délais.
-    <br/>    <br/>
-    Pour accéder rapidement à l’ensemble de nos formations programmées, <a href='$baseUrl/sessions/'>rendez-vous sur notre site</a>.
-    <br/>    <br/>
-    Par ailleurs, si vous souhaitez vous inscrire directement à une formation, vous pouvez :
-      <ul>
-      <li>Par internet : rendez-vous sur notre site, dans la rubrique <a href='$baseUrl/infos-pratiques/'>INFOS PRATIQUES</a> pour télécharger le bulletin d’inscription ou procédez à l’inscription en ligne directement à partir du programme de formation qui vous intéresse</li>
-      <li>Par téléphone : 01.47.59.87.95</li>
-      <li>Par e-mail : envoyez votre bulletin dûment rempli, signé et portant le cachet de l’entreprise à <a href='mailto:formations@arcphar.com'>formations@arcphar.com</a></li>
-      <li>Par fax : renvoyez votre bulletin d’inscription, signé et portant le cachet de l’entreprise au 01.47.59.87.88</li>
-      <li>Par courrier : vous pouvez renvoyer votre bulletin d’inscription signé et portant le cachet de l’entreprise à A.R.C. Pharma Formations 41 rue de Villiers, 92523 Neuilly-sur-Seine Cedex.</li>
-      </ul>
-      <br/>    <br/>
-      Cordialement,
-      <br/>
-      <br/>
-      <br/>
-      -- <br/>
-      L’équipe A.R.C. Pharma Formations<br/>
-      Tel : 01.47.59.87.95<br/>
-      <a href='mailto:formations@arcphar.com'>formations@arcphar.com</a> <br/>
-      <a href='www.arcphar-formations.com'>www.arcphar-formations.com</a>
-      ";
-
-      wp_mail($email, $internauteEmailSujet,$internauteEmailBody,$headers);
-
-    }
-
-
-
-  }
-
-  ?>
+?>
--- templates/components/pagination/template-pagination-next.php
+++
@@ -1,7 +0,0 @@
-<?php if(get_previous_posts_page_link() && is_paged()): ?>
-  <li><a href="<?= get_previous_posts_page_link();?>">&larr; Précédent</a></li>
-<?php endif; ?>
-
-<?php $url = get_next_posts_page_link(); if($url !== "" && Helpers::others_page_exists()): ?>
-  <a href="<?=$url;?>">Suivant &rarr;</a>
-<?php endif; ?>
